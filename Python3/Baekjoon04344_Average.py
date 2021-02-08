8d0126e2bf696afbc1d741983155aa5bf4496f93
podo <76838814+popopododo@users.noreply.github.com>
2021-02-09 02:27:03 +0900

    Array in Baekjoon

--- Python3/Baekjoon04344_Average.py
+++ Python3/Baekjoon04344_Average.py
@@ -1,10 +1,15 @@
 T=int(input())
 
-while(T<0):
+while(1):
     scorelist=list(map(int,input().split()))
-    subject=scorelist.pop()
+    subject=scorelist.pop(0)
     total=0
+    count=0
     for x in scorelist:
         total+=x
-    print('{}%'.format(round(total/subject,3)),end="\n")
+    average=round(total/subject,3)
+    for x in scorelist:
+        if x>average:
+            count+=1
+    print('{}%'.format(round(count/subject*100,3)),end="\n")
     T=-1
